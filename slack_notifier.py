789df76f8a00da9bc6615fc3d047324894a0ba46
just-another-user <thisismyregularmail@gmail.com>
2022-04-16 21:49:37 +0300

    Added the ability to send a block of text instead of plain text

--- slack_notifier.py
+++ slack_notifier.py
@@ -5,17 +5,34 @@ import requests
 class SlackNotifier:
 	chat_post_message_url = "https://slack.com/api/chat.postMessage"
 
+	markdown_block = {
+		"blocks": [
+			{
+				"type": "section",
+				"text": {
+					"type": "mrkdwn",
+					"text": ""
+				}
+			}
+		]
+	}
+
 	def __init__(self, token):
 		self.headers = {
 			"Content-Type": "application/json; charset=utf-8",
 			"Authorization": f"Bearer {token}"
 		}
 
-	def post_message(self, channel_id, text):
+	def _create_block(self, text):
+		b = self.markdown_block
+		b["blocks"][0]["text"]["text"] = text
+		return b
+
+	def post_message(self, channel_id, text, is_markdown=False):
 		msg = {
 			"channel": channel_id,
-			"text": text
 		}
+		msg.update(self._create_block(text) if is_markdown else {"text": text})
 		r = requests.post(self.chat_post_message_url, data=json.dumps(msg), headers=self.headers)
 		if r.status_code != 200:
 			print(json.dumps(r.json(), indent=2))
@@ -30,6 +47,7 @@ if __name__ == '__main__':
 	parser.add_argument("msg", action="store", type=str, help="The message to send")
 	parser.add_argument("-t", "--token", action="store", type=str,
 						help="Bearer token for slack. If not provided, extracted from 'slackToken' environment variable")
+	parser.add_argument("-m", "--markdown", action="store_true", help="Send the message as a markdown block")
 	args = parser.parse_args()
 
 	slack_token = args.token or os.environ.get("slackToken")
@@ -37,4 +55,4 @@ if __name__ == '__main__':
 		print("Error: No token provided. You must provide a slack token in order to post messages.")
 	else:
 		sn = SlackNotifier(slack_token)
-		sn.post_message(args.cid, args.msg)
+		sn.post_message(args.cid, args.msg, args.markdown)
